340585cb5e35cb20d8f8d6efeb140c2b69a8719a
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-11-08 21:44:24 +0600

    refactor: Updated surfingKeys, npmScript/npmScript.conf, shellScript/rofi-cliphist.sh

--- zshScript/shellScript/rofi-cliphist.sh
+++ zshScript/shellScript/rofi-cliphist.sh
@@ -3,7 +3,6 @@
 selected=$(cliphist list | rofi -dmenu -i -p "📋 Clipboard" | cut -d' ' -f1)
 
 if [[ -n "$selected" ]]; then
-    cliphist decode "$selected" | wl-copy --type text/plain
-    notify-send "✅ Copied to clipboard"
+  cliphist decode "$selected" | wl-copy --type text/plain
+  # notify-send "✅ Copied to clipboard"
 fi
-
