a7b0c1fb5a30737619f9756e1ac4ae52816db7f0
Backend Shahjalal <shahjalal.backend@gmail.com>
2025-12-22 15:46:59 +0600

    feat: Added shellscript/killPanesExceptFirst3Prev.sh | refactor: Updated tmuxZshCli/tmuxZshCli.conf | chore: Removed shellscript/killPanesExceptFirst3.sh

---
+++ tmuxModule/tmuxZshCli/shellscript/killPanesExceptFirst3Prev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# kill all panes except the first 3 => which are index 1,2,3
+
+# Get pane index + pane id pairs
+# Format: index:id
+panes=$(tmux list-panes -F '#{pane_index}:#{pane_id}')
+
+# Loop through panes
+for pane in $panes; do
+  index="${pane%%:*}"
+  id="${pane##*:}"
+
+  # Skip first three panes (index starts from 1)
+  if [ "$index" -gt 3 ]; then
+    tmux kill-pane -t "$id"
+  fi
+done
